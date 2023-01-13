bf7f5e5cc588f80961c2a6e6df4cf033ba14cca6
canubemy2meow <70944427+61010822@users.noreply.github.com>
2023-01-13 13:33:42 +0700

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -7,4 +7,4 @@ echo "******************************"
 echo "*** Building Docker Images ***"
 echo "******************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && ls && docker-compose -f docker-compose-build.yml build --no-cache
