0362a5e541014785a7b7bbef83301eddf6ff2c5c
canubemy2meow <70944427+61010822@users.noreply.github.com>
2023-01-13 12:14:59 +0700

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "**********************"
 echo "**** Building Jar *****"
 echo "**********************"
 
-WORKSPACE=/home/pittimon/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/jenkins/workspace/pipeline-docker-maven
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.8.6-eclipse-temurin-11-alpine "$@" 
